3a70d90ddb50c7dc07d374c08d785ec557d55bb3
UIT-20521205 <dinhiendung1026@gmail.com>
2023-12-16 21:18:49 +0700

    fix ddeploy

--- InstaTalk/Models/RoomModel.cs
+++ InstaTalk/Models/RoomModel.cs
@@ -6,14 +6,12 @@ namespace InstaTalk.Models
     {
         public Guid RoomId { get; set; }
         public string RoomName { get; set; }
-
-        public string SecurityCode { get; set; }
-        public int CountMember { get; set; }
         public Guid UserId { get; set; }
         public string UserName { get; set; }
         public string DisplayName { get; set; }
+        public int CountMember { get; set; }
         public DateTime CreatedDate { get; set; }
-        public bool BlockedChat { get; set; }
+        public bool BlockedChat { get; set; } = false;
 
     }
 }
