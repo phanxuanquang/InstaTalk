3193c3967ec4a8f7aa2f4dfed4996a7ee7569e71
ThietTV <thiettv02@gmail.com>
2023-12-16 17:58:00 +0700

    (...) fix display name

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -5,11 +5,6 @@ namespace API.Extensions
 {
     public static class ClaimsPrincipleExtensions
     {
-        public static string GetUsername(this ClaimsPrincipal user)
-        {
-            return user.FindFirst(ClaimTypes.Name)?.Value;
-        }
-
         public static Guid GetUserId(this ClaimsPrincipal user)
         {
             return Guid.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier));
@@ -17,7 +12,7 @@ namespace API.Extensions
 
         public static string GetDisplayName(this ClaimsPrincipal user)
         {
-            return user.FindFirstValue(ClaimTypes.Name);
+            return user.FindFirstValue("name");
         }
     }
 }
