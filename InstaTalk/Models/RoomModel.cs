0b342e8a7f42b31a58704ae82dc43d83e3ee3060
UIT-20521205 <dinhiendung1026@gmail.com>
2023-12-16 21:14:32 +0700

    fix ddeploy

--- InstaTalk/Models/RoomModel.cs
+++ InstaTalk/Models/RoomModel.cs
@@ -1,5 +1,4 @@
-﻿
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
 namespace InstaTalk.Models
 {
@@ -14,7 +13,6 @@ namespace InstaTalk.Models
         public string UserName { get; set; }
         public string DisplayName { get; set; }
         public DateTime CreatedDate { get; set; }
-        [DefaultValue(false)]
         public bool BlockedChat { get; set; }
 
     }
