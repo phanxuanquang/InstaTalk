dffd51d62a4a704f0bd9ed4f0c1b37639fe97bc5
phucnh20521768 <nhphuc0703@gmail.com>
2024-01-08 23:48:05 +0700

    call join stranger

--- InstaTalk/wwwroot/js/waiting.js
+++ InstaTalk/wwwroot/js/waiting.js
@@ -5,4 +5,15 @@ strangerService.createHubConnection(ObjClient.User, ObjClient.Room.roomId);
 strangerService.joinStranger$.subscribe(roomId => {
     let name = document.getElementById("stranger_display_name");
     name.innerHTML = roomId;
+    var postData = {
+        RoomId: roomId,
+        SecurityCode: "",
+    };
+    fetch("/Stranger/Matching", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+    })
 });
