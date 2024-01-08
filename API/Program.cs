50954ef55a19ab1e922696d1dbda11f4515ba9c6
ThietTV <thiettv02@gmail.com>
2024-01-09 01:47:31 +0700

    (...) remove production swagger

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || true)
+if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
